5507e12a92e00ec30841649e56f77773f6e1434a
Yasin <6695727+yasin-ce@users.noreply.github.com>
2025-10-30 06:41:14 +0100

    Embed lazysodium lib

---
+++ lazysodium-java/build.gradle.kts
@@ -0,0 +1,86 @@
+plugins {
+    `java-library`
+    `maven-publish`
+    signing
+}
+
+group = "app.perawallet"
+version = "5.1.5"
+description = "Lazysodium (Java) makes it effortless for Java developers to get started with Libsodium's cryptography."
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+    withSourcesJar()
+    withJavadocJar()
+}
+
+sourceSets {
+    named("main") {
+        resources {
+            srcDirs("src/main/resources", "src/main/jniLibs")
+        }
+    }
+}
+
+dependencies {
+    implementation("net.java.dev.jna:jna:5.18.1")
+    implementation("com.goterl:resource-loader:2.1.0")
+    implementation("org.slf4j:slf4j-api:2.0.17")
+}
+
+tasks.withType<Jar>().configureEach {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest {
+        attributes["Automatic-Module-Name"] = "app.perawallet.lazysodium"
+    }
+}
+
+tasks.withType<Javadoc>().configureEach {
+    options.encoding = "UTF-8"
+    (options as StandardJavadocDocletOptions).addStringOption("Xdoclint:none", "-quiet")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("release") {
+            from(components["java"])
+            groupId = project.group.toString()
+            artifactId = "lazysodium-java"
+            version = project.version.toString()
+
+            pom {
+                name.set("Lazysodium Java")
+                description.set(project.description)
+                url.set("https://github.com/terl/lazysodium-java")
+                licenses {
+                    license {
+                        name.set("Apache License 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("goterl")
+                        name.set("Goterl Team")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/terl/lazysodium-java.git")
+                    developerConnection.set("scm:git:ssh://github.com/terl/lazysodium-java.git")
+                    url.set("https://github.com/terl/lazysodium-java")
+                }
+            }
+        }
+    }
+}
+
+signing {
+    if (System.getenv("GPG_PRIVATE_KEY") != null) {
+        useInMemoryPgpKeys(
+            System.getenv("GPG_PRIVATE_KEY"),
+            System.getenv("GPG_PRIVATE_KEY_PASSWORD")
+        )
+        sign(publishing.publications)
+    }
+}
---
+++ lazysodium-java/src/main/java/app/perawallet/lazysodium/LazySodiumAndroid.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) Terl Tech Ltd • 04/04/2020, 00:05 • goterl.com
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v2.0. If a copy of the MPL was not distributed with this
+ * file, you can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package app.perawallet.lazysodium;
+
+import app.perawallet.lazysodium.interfaces.MessageEncoder;
+import app.perawallet.lazysodium.utils.HexMessageEncoder;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class LazySodiumAndroid extends LazySodium {
+
+    private final SodiumAndroid sodium;
+
+    public LazySodiumAndroid(SodiumAndroid sodium) {
+        this(sodium, StandardCharsets.UTF_8, new HexMessageEncoder());
+    }
+
+    public LazySodiumAndroid(SodiumAndroid sodium, Charset charset) {
+        this(sodium, charset, new HexMessageEncoder());
+    }
+
+    public LazySodiumAndroid(SodiumAndroid sodium, MessageEncoder messageEncoder) {
+        this(sodium, StandardCharsets.UTF_8, messageEncoder);
+    }
+
+    public LazySodiumAndroid(SodiumAndroid sodium, Charset charset, MessageEncoder messageEncoder) {
+        super(charset, messageEncoder);
+        this.sodium = sodium;
+    }
+
+    public SodiumAndroid getSodium() {
+        return sodium;
+    }
+
+}
